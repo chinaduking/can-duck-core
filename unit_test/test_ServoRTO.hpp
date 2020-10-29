a95ff5236456ad3fcdd46313bae232f310f5ebec
Shiqian Dong <dongshiqian88@sina.com>
2020-10-29 20:00:18 +0800

    move RTO init

--- unit_test/test_ServoRTO.hpp
+++ unit_test/test_ServoRTO.hpp
@@ -30,10 +30,26 @@ namespace fcnmsg{
         target_angle(3),
         mode    (4)
         {
-            init();
+            obj_base_offset[speed.index] = (uint8_t *)&speed
+                                           - (uint8_t*)p_first_obj;
+            obj_base_offset[angle.index] = (uint8_t *)&angle
+                                           - (uint8_t*)p_first_obj;
+            obj_base_offset[current.index] = (uint8_t *)&current
+                                             - (uint8_t*)p_first_obj;
+            obj_base_offset[target_angle.index] = (uint8_t *)&target_angle
+                                                  - (uint8_t*)p_first_obj;
+            obj_base_offset[mode.index] = (uint8_t *)&mode
+                                          - (uint8_t*)p_first_obj;
+
+            Buffer buffer_tmp;
+
+            speed.buffer_offset = (uint8_t *)&buffer_tmp.speed - (uint8_t *)&buffer_tmp;
+            angle.buffer_offset = (uint8_t *)&buffer_tmp.angle - (uint8_t *)&buffer_tmp;
+            current.buffer_offset = (uint8_t *)&buffer_tmp.current - (uint8_t *)&buffer_tmp;
+            target_angle.buffer_offset = (uint8_t *)&buffer_tmp.target_angle - (uint8_t *)&buffer_tmp;
+            mode.buffer_offset = (uint8_t *)&buffer_tmp.mode - (uint8_t *)&buffer_tmp;
         }
 
-        void init();
 
         struct Buffer{
             int16_t speed;
