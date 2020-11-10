c9135bb2045f5481e854c300465d61815e859bc9
Shiqian Dong <dongshiqian88@sina.com>
2020-11-10 12:00:54 +0800

    update logview

--- src/apps/SerialLogViewer/main.cpp
+++ src/apps/SerialLogViewer/main.cpp
@@ -2,19 +2,54 @@
 // Created by sdong on 2020/11/2.
 //
 #include "utils/os_only/PosixSerial.hpp"
-
+#include <cstring>
+#include <iostream>
 using namespace utils;
-
-int main(){
-    PosixSerial serial(0, B921600);
-    uint8_t data;
+using namespace std;
+int main( int argc, char *argv[]){
     /* 0. ascii code
      * 1. hex  */
     uint8_t mode = 1;
 
+    if(argc >= 2){
+        if(strcmp(argv[1], "s") == 0){
+            cout << "logview: ascii char string mode " << endl;
+            mode = 0;
+        }
+        else if(strcmp(argv[1], "h") == 0){
+            cout << "logview: hex mode " << endl;
+
+            mode = 1;
+        } else{
+            cerr << "param is s or h" << endl;
+            exit(-1);
+        }
+    }else{
+        cout << "logview: hex mode (defualt)" << endl;
+    }
+
+    int sid = 0;
+
+    if(argc >= 3){
+        sid = argv[2][0] - '0';
+
+        if(sid < 0){
+            cerr << "error: sid < 0" << endl;
+            exit(-1);
+        }
+
+        if(sid < 5){
+            cerr << "error: sid > 5" << endl;
+            exit(-1);
+        }
+    }
+
+    PosixSerial serial(0, B921600);
+
 
     uint8_t line_wrap = 20;
     uint8_t line_wrap_cnt = 0;
+    uint8_t data;
 
     while(serial.isOpen()){
         auto i = serial.read(&data, 1);
